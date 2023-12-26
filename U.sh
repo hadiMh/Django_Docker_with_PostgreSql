4dda329de37ec5c70d7ecbbc5a0dcc0e50b9507d
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 06:04:30 -0500

    💠 add accounts to INSTALLED_APPS in config/settings.py

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 Second add accounts directory "
+git commit -m " 💠 add accounts to INSTALLED_APPS in config/settings.py "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
