a5dfac4b042a1c5cd4cbd8755c2c7e8a1a80cdf3
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 09:35:04 -0500

    💠 Create Super user and check that

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 Debug all of these"
+git commit -m "💠 Create Super user and check that"
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
