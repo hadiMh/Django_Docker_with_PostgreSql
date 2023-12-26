fe07312642437e8ebd6c9037f985a62d3815c88e
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 06:30:05 -0500

    💠Create CustomUserCreationForm class into accounts/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',  )
+
 
 
 
