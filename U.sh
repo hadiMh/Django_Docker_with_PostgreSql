2e832078fa648973c2c0d3e64c6d25f5f8c9ab51
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 16:04:38 -0500

    💠 config urls.py in pages directory

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 config url route to pages urls.py "
+git commit -m "💠 config urls.py in pages directory "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ]
+
+
+
