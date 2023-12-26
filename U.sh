82c3ec5ee3f9f5ceccd6b641867b9a2ad110ac93
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 06:33:12 -0500

    💠Add Setting into config/settings.py

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠Create CustomUserCreationForm class into accounts/forms.py"
+git commit -m "💠Add Setting into config/settings.py"
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+
+
+
+
+
+
+
+
+
+
+
