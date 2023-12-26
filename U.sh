a37af1b6653f4e69395f55c61c37cd7e5fa8ace3
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 06:23:41 -0500

    💠Create forms.py file in accounts directory

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m " 💠 rechange all steps "
+git commit -m "💠Create forms.py file in accounts directory"
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
