aa0c186151b7155f6143cc51f3d0ae730a225d3c
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 07:08:08 -0500

    💠Cunfig somthings for admin panel Django into accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,15 @@ from django.contrib.auth.admin import UserAdmin
 
 
 from .models import CustomUser
+from .forms import CustomUserCreationForm
+from .forms import CustomUserChangeForm
 
-
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email', 'username',  )
+    
 
 
