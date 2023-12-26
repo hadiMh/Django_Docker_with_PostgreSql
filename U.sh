791c4d8b287030987861f5fa347879e2ae443eb4
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 06:25:59 -0500

    💠Import these to in accounts/forms.py

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠Create forms.py file in accounts directory"
+git commit -m "💠Import these to in accounts/forms.py"
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+
+
+
+
+
