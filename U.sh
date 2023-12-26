48de5bc35d8a34b6c8df0f10afb3195b2833232e
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-26 07:26:50 -0500

    💠 Debug all of these

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠Cunfig somthings for admin panel Django into accounts/admin.py"
+git commit -m "💠 Debug all of these"
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
-
+from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -12,7 +12,7 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username',  )
-        
+
 
 
 
