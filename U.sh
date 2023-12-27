32b5912b181e48cab7dcea3e69de58b23492995d
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-27 15:46:49 -0500

    💠 42- create new class for handle view in accounts/views.py

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 40- add urlpatterns in accounts/urls.py "
+git commit -m "💠 42- create new class for handle view in accounts/views.py "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+    
+
+
