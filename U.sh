f0833244c62f4c485074ff0512da5f75bf10bf39
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-27 15:38:07 -0500

    💠 38- Create urls.py in accounts directory

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 36- Check these links and should be shows you wrote in home.html and aboutus.html "
+git commit -m "💠 38- Create urls.py in accounts directory "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 
