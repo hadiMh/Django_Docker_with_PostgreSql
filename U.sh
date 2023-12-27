ff65c82dafdbfefed282cb006a798fac6537cd5a
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-27 15:39:22 -0500

    💠 39- import this in accounts/urls.py

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 38- Create urls.py in accounts directory "
+git commit -m "💠 39- import this in accounts/urls.py "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
