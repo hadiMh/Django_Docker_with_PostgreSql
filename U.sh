f4799b954258c800b458f1c3daa09df8c5d8c872
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-28 17:50:57 -0500

    💠 46- add this in templates/registration/login.html

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 43- Create this in templates directory "
+git commit -m "💠 46- add this in templates/registration/login.html "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
