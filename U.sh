8d4f9238685a35dc7b532fdbc95628557ca96ecc
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-28 17:54:59 -0500

    💠 47- Check site , write in Terminal , check these links should be work

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 46- add this in templates/registration/login.html "
+git commit -m "💠 47- Check site , write in Terminal , check these links should be work "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
