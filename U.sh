c10b117655d123f531fbb84d2a1c923c028f84b6
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-30 03:03:51 -0500

    💠 52- write this in Terminal

--- U.sh
+++ U.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "💠 50- Check these links after signup and logout  "
+git commit -m "💠 52- write this in Terminal "
 git push -u origin sinaLaleh
 
 # with ScreenShot for showing demo png file
