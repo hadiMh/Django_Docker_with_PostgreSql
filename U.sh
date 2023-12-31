8978ccb5ffa8f5c1a1ebec662af92b143026b668
sina lalehbakhsh <lalebakhshsina@gmail.com>
2023-12-31 02:17:32 -0500

    💠💠💠 For All Projects 💠💠💠

---
+++ U.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git add .
+git commit -m " 💠💠💠 For All Projects 💠💠💠 "
+git push -u origin main 
+
+# with ScreenShot for showing demo png file
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    
+    # Custom Created
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accounts config
+AUTH_USER_MODEL =  'accounts.CustomUser'
